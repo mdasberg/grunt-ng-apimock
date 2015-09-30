ad6503f985f5a66ddd717b9d9f7a679cc235423f
Mischa Dasberg <mischa@dasberg.nl>
2015-09-30 19:51:15 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+(function () {
+    /*
+     * grunt-ng-apimock
+     * https://github.com/mdasberg/grunt-ng-apimock
+     *
+     * Copyright (c) 2015 Mischa Dasberg
+     * Licensed under the MIT license.
+     */
+
+    'use strict';
+
+    module.exports = function (grunt) {
+
+        // Project configuration.
+        grunt.initConfig({
+                jshint: {
+                    all: [
+                        'Gruntfile.js',
+                        'tasks/*.js'
+                    ],
+                    options: {
+                        jshintrc: '.jshintrc'
+                    }
+                },
+                ngApimock: {
+                    options: {},
+                    mock: {
+                        src: 'mocks/**/*.js',
+                        dest: 'mock.js'
+                    }
+                },
+
+                // Before generating any new files, remove any previously-created files.
+                clean: {
+                    tests: ['.tmp']
+                },
+
+                shell: {
+                    target: {
+                        command: 'node_modules/jasmine-node/bin/jasmine-node test/*.spec.js'
+                    }
+                }
+            }
+        );
+
+        // Actually load this plugin's task(s).
+        grunt.loadTasks('tasks');
+
+        // These plugins provide necessary tasks.
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-shell');
+
+        // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+        // plugin's task(s), then test the result.
+        grunt.registerTask('test', ['clean', 'shell']);
+
+        // By default, lint and run all tests.
+        grunt.registerTask('default', ['jshint', 'test']);
+
+    };
+})();
---
+++ tasks/ngApimock.js
@@ -0,0 +1,16 @@
+(function () {
+    /*
+     * grunt-ng-apimock
+     * https://github.com/mdasberg/grunt-ng-apimock
+     *
+     * Copyright (c) 2015 Mischa Dasberg and contributors
+     * Licensed under the MIT license.
+     */
+
+    'use strict';
+    module.exports = function (grunt) {
+        /** Sonar karma results. */
+        grunt.registerMultiTask('ngApimock', 'Grunt plugin that provides the ability to use scenario based api mocking of angular apps",', function () {
+        });
+    };
+})();
