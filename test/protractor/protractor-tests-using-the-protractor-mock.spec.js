41439c5b7a0cb2c404de4b9dc6dbc4c0cbeff772
Mischa Dasberg <mischa@dasberg.nl>
2015-11-26 11:55:02 +0100

    Add tests for changing scenarios at runtime

--- test/protractor/protractor-tests-using-the-protractor-mock.spec.js
+++ test/protractor/protractor-tests-using-the-protractor-mock.spec.js
@@ -78,6 +78,50 @@
             });
         });
 
+        describe('when changing some selected scenarios', function () {
+            beforeAll(function () {
+                ngApimock.selectScenario(require('../mocks/some-api-get.json'), 'some-meaningful-scenario-name');
+                ngApimock.selectScenario(require('../mocks/some-api-post.json'), 'successful');
+                ngApimock.addMockModule();
+                browser.get('/index.html');
+            });
+
+            describe('when fetching data with a service', function() {
+                it('should show data', function () {
+                    expect(element(by.binding('ctrl.data')).getText()).toBe('[{"x":"y"}]');
+                    ngApimock.selectScenario(require('../mocks/some-api-get.json'), 'another-meaningful-scenario-name');
+                    element(by.buttonText('refresh')).click();
+                    expect(element(by.binding('ctrl.data')).getText()).toBe('[{"a":"b"}]');
+                });
+
+                it('should not show any errors', function () {
+                    expect(element(by.binding('ctrl.error')).getText()).toBe('');
+                });
+            });
+
+            describe('when posting data with a service', function() {
+                beforeAll(function() {
+                    element(by.buttonText('post me')).click();
+                });
+
+                it('should show data', function () {
+                    expect(element(by.binding('ctrl.postedData')).getText()).toBe('{"some":"thing"}');
+                    ngApimock.selectScenario(require('../mocks/some-api-post.json'), 'internal-server-error');
+                    element(by.buttonText('post me')).click();
+                    expect(element(by.binding('ctrl.postedData')).getText()).toBe('');
+                });
+
+                it('should not show any errors', function () {
+                    expect(element(by.binding('ctrl.postedError')).getText()).toBe('500');
+                });
+            });
+
+            afterAll(function () {
+                ngApimock.removeMockModule();
+                ngApimock.resetScenarios();
+            });
+        });
+
         describe('when provided with some selected error scenarios', function () {
 
             beforeAll(function () {
